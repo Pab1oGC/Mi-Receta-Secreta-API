84bf3cbfaf44773f358e446dc7fce963c95dfce6
Pablo Gutierrez P <pablogutierrrez0@gmail.com>
2024-11-21 00:25:31 -0400

    Create project

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiRecetaSecretaAPI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options) { }
+
+
+    }
+}
