3f0c52a5df40460bd9c3eb24f0ff662954de2948
Pablo Gutierrez P <pablogutierrrez0@gmail.com>
2024-12-01 22:22:36 -0400

    Crud ingredients finished

--- Controllers/IngredientsController.cs
+++ Controllers/IngredientsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MiRecetaSecretaAPI.Data;
 using MiRecetaSecretaAPI.Models;
@@ -7,6 +8,7 @@ namespace MiRecetaSecretaAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("SecretRecipeFront")]
     public class IngredientsController : Controller
     {
         private readonly AppDBContext _db;
